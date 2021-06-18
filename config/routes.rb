01a7bea5b857d48868b8db30464ff961ddb4e382
essbee808 <sheena.myb@gmail.com>
2021-06-18 10:30:50 -0700

    create coupon controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'coupon/new'
+
+  get 'coupon/create'
+
+  get 'coupon/show'
+
+  get 'coupon/index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
