b392d502b29a8e28805f91819ee5a6a693aff568
essbee808 <sheena.myb@gmail.com>
2021-06-18 11:52:31 -0700

    add link on index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
-  get 'coupon/new'
 
-  get 'coupon/create'
+ resources :coupons
 
-  get 'coupon/show'
-
-  get 'coupon/index'
+ 
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
